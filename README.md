605ce5799ea0951c476673183696f71399a0c161
celeri <celestin.bouchet@gmail.com>
2025-07-17 18:50:44 +0200

    Custom base user + update README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # testvault
 
+This project needs jdk 21 or higher.
+
 ## Setup
 
 Start vault/bao and mariadb and create a testvault database with its associated user (or use the given docker compose file).
@@ -7,7 +9,13 @@ Start vault/bao and mariadb and create a testvault database with its associated 
 Store the database connection information in vault/bao:
 
 ```shell
-bao kv put secret/testvault DATABASE_HOST=<HOST> DATABASE_PORT=<PORT> DATABASE_USER=<USER> DATABASE_PASS=<PASS>
+bao kv put secret/testvault DATABASE_HOST="<HOST>" DATABASE_PORT="<PORT>" DATABASE_USER="<USER>" DATABASE_PASS="<PASS>"
+```
+
+Store the connection information of the first user created by the application in vault/bao:
+
+```shell
+bao kv patch secret/testvault TESTVAULT_USERNAME="<USER>" TESTVAULT_PASSWORD="<PASS>"
 ```
 
 Add the vault/bao token in the `src/resources/application.yml` file:
@@ -28,3 +36,12 @@ Start the application with:
 ```shell
 ./gradlew bootRun
 ```
+
+You can now access the application's generated swagger at <http://localhost:8080/swagger-ui/index.html>.
+
+## Usage
+
+You can login using the `/login` endpoint in the swagger or via the form at <http://localhost:8080/login>.
+
+After that you can use all the endpoints listed in the swagger file, depending on your user's roles.
+The base user has `ADMIN` and `USER` roles.
--- src/main/kotlin/net/celeri/testvault/TestvaultApplication.kt
+++ src/main/kotlin/net/celeri/testvault/TestvaultApplication.kt
@@ -5,6 +5,7 @@ import net.celeri.testvault.models.UserCreate
 import net.celeri.testvault.services.UserService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -13,15 +14,21 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class TestvaultApplication(
     private val userService: UserService,
+    @Value("\${app.base-user.username}") private val baseUserName: String,
+    @Value("\${app.base-user.password}") private val baseUserPassword: String?,
 ) : ApplicationRunner {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun run(args: ApplicationArguments?) {
+        if (baseUserPassword == null || baseUserPassword.isEmpty()) {
+            throw IllegalStateException("Please provide a password for the application's base user via the TESTVAULT_PASSWORD env variable")
+        }
+
         try {
-            userService.createUser(UserCreate("root", "root", setOf(Role.USER, Role.ADMIN)))
-            logger.info("root user created")
-        } catch (e: IllegalStateException) {
-            logger.info("root user already created")
+            userService.createUser(UserCreate(baseUserName, baseUserPassword, setOf(Role.USER, Role.ADMIN)))
+            logger.info("base user created")
+        } catch (_: IllegalStateException) {
+            logger.info("base user already created")
         }
     }
 }
